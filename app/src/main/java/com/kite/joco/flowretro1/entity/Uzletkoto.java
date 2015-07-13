cda5d6a9fa91cdf344fa6177ca5c0b8de3fccc09
mesterj <mesterj@freemail.hu>
2015-07-13 16:16:15 +0200

    Initial commit

---
+++ app/src/main/java/com/kite/joco/flowretro1/entity/Uzletkoto.java
@@ -0,0 +1,18 @@
+package com.kite.joco.flowretro1.entity;
+
+/**
+ * Created by Mester József on 2015.07.13..
+ */
+public class Uzletkoto {
+
+    String szat;
+    String nev;
+    String email;
+    String alkozpont;
+    String alkrovidkod;
+    String ugyintezo;
+    String ugyiemail;
+    String lastsorszam;
+    String lastpdfazon;
+
+}
