467f729a76c142c3b223884211f4cb7e8270ceec
mesterj <mesterj@freemail.hu>
2015-07-30 20:55:13 +0200

    sync service started

---
+++ app/src/main/java/com/kite/joco/flowretro1/syncservice/Syncservice.java
@@ -0,0 +1,15 @@
+package com.kite.joco.flowretro1.syncservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by Joco on 2015.07.30..
+ */
+public class Syncservice extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
